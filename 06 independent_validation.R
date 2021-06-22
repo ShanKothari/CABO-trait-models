01516886a09b6bcd2ef503c82da13a6a11338513
Shan Kothari <quercusacerifolia@gmail.com>
2021-06-22 18:51:37 -0400

    RMSD, fix axis labels

--- 06 independent_validation.R
+++ 06 independent_validation.R
@@ -23,6 +23,21 @@ interpolate <- function(x) {
   return(tmp)
 }
 
+## RMSD between predicted and observed values
+RMSD<-function(measured,predicted){
+  not.na<-which(!is.na(measured) & !is.na(predicted))
+  return(sqrt(sum((measured-predicted)^2,na.rm=T)/(length(not.na)-1)))
+}
+
+## percent RMSD (based on data quantiles)
+## set min and max to 0 and 1 for range as denominator
+## or to 0.25 and 0.75 for IQR as denominator
+percentRMSD<-function(measured,predicted,min,max,na.rm=T){
+  RMSD_data<-RMSD(measured,predicted)
+  range<-quantile(measured,probs=max,na.rm=na.rm)-quantile(measured,probs=min,na.rm=na.rm)
+  return(RMSD_data/range)
+}
+
 ## applying coefficients to validation spectra
 apply.coefs<-function(coef.list,val.spec,intercept=T){
   if(sum(lapply(coef.list,length)==ncol(val.spec)+intercept) < length(coef.list)){
@@ -635,7 +650,7 @@ Ca_ind_val<-ggplot(data=meta(all_val_ref),aes(x=Ca_pred,y=Ca_mass,color=dataset)
   theme(text = element_text(size=20))+
   geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
   geom_smooth(method="lm")+
-  labs(y="Measured Al",x="Predicted Al")+
+  labs(y="Measured Ca",x="Predicted Ca")+
   coord_cartesian(xlim=c(-5,30),ylim=c(-5,30))
 
 Cu_ind_val<-ggplot(data=meta(all_val_ref),aes(x=Cu_pred,y=Cu_mass,color=dataset))+
