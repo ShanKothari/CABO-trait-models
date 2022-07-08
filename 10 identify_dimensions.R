cd432ac974f026a4c797532fa6cf4902a4d1ab46
Shan Kothari <quercusacerifolia@gmail.com>
2022-07-08 00:50:56 -0400

    include norm-ind and trait PCA
    + delete dim estimation methods not used
    + change which herbarium paper data used
    + more documentation

--- 10 identify_dimensions.R
+++ 10 identify_dimensions.R
@@ -28,77 +28,142 @@ meta(ref.traits)$chl_area<-meta(ref.traits)$chl_mass*meta(ref.traits)$LMA/10000
 
 ###################################
 ## calculate normalization-independent traits
-Narea_norm<-lm(log10(Narea)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Nnorm<-resid(Narea_norm)
 
-Carea_norm<-lm(log10(Carea)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Cnorm<-resid(Carea_norm)
+Narea_norm<-lm(log10(Narea)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Nnorm<-resid(Narea_norm)
 
-solubles_area_norm<-lm(log10(solubles_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$solubles_norm<-resid(solubles_area_norm)
+Carea_norm<-lm(log10(Carea)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Cnorm<-resid(Carea_norm)
 
-hemicellulose_area_norm<-lm(log10(hemicellulose_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$hemicellulose_norm<-resid(hemicellulose_area_norm)
+solubles_area_norm<-lm(log10(solubles_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$solubles_norm<-resid(solubles_area_norm)
 
-cellulose_area_norm<-lm(log10(cellulose_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$cellulose_norm<-resid(cellulose_area_norm)
+hemicellulose_area_norm<-lm(log10(hemicellulose_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$hemicellulose_norm<-resid(hemicellulose_area_norm)
 
-lignin_area_norm<-lm(log10(lignin_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$lignin_norm<-resid(lignin_area_norm)
+cellulose_area_norm<-lm(log10(cellulose_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$cellulose_norm<-resid(cellulose_area_norm)
 
-chlA_area_norm<-lm(log10(chlA_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$chlA_norm<-resid(chlA_area_norm)
+lignin_area_norm<-lm(log10(lignin_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$lignin_norm<-resid(lignin_area_norm)
 
-chlB_area_norm<-lm(log10(chlB_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$chlB_norm<-resid(chlB_area_norm)
+chlA_area_norm<-lm(log10(chlA_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$chlA_norm<-resid(chlA_area_norm)
+
+chlB_area_norm<-lm(log10(chlB_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$chlB_norm<-resid(chlB_area_norm)
 
 chl_area_norm<-lm(log(chl_area)~log(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
 meta(ref.traits)$chl_norm<-resid(chl_area_norm)
 
-car_area_norm<-lm(log10(car_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$car_norm<-resid(car_area_norm)
+car_area_norm<-lm(log10(car_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$car_norm<-resid(car_area_norm)
 
-meta(all.ref)$Al_area_omit<-meta(all.ref)$Al_area
-meta(all.ref)$Al_area_omit[which(meta(all.ref)$Al_area_omit==0)]<-NA
-Al_area_norm<-lm(log10(Al_area_omit)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Al_norm<-resid(Al_area_norm)
-meta(all.ref)$Al_area_omit<-NULL
+meta(ref.traits)$Al_area_omit<-meta(ref.traits)$Al_area
+meta(ref.traits)$Al_area_omit[which(meta(ref.traits)$Al_area_omit==0)]<-NA
+Al_area_norm<-lm(log10(Al_area_omit)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Al_norm<-resid(Al_area_norm)
+meta(ref.traits)$Al_area_omit<-NULL
 
-Ca_area_norm<-lm(log10(Ca_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Ca_norm<-resid(Ca_area_norm)
+Ca_area_norm<-lm(log10(Ca_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Ca_norm<-resid(Ca_area_norm)
 
-meta(all.ref)$Cu_area_omit<-meta(all.ref)$Cu_area
-meta(all.ref)$Cu_area_omit[which(meta(all.ref)$Cu_area_omit==0)]<-NA
-Cu_area_norm<-lm(log10(Cu_area_omit)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Cu_norm<-resid(Cu_area_norm)
-meta(all.ref)$Cu_area_omit<-NULL
+meta(ref.traits)$Cu_area_omit<-meta(ref.traits)$Cu_area
+meta(ref.traits)$Cu_area_omit[which(meta(ref.traits)$Cu_area_omit==0)]<-NA
+Cu_area_norm<-lm(log10(Cu_area_omit)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Cu_norm<-resid(Cu_area_norm)
+meta(ref.traits)$Cu_area_omit<-NULL
 
-Fe_area_norm<-lm(log10(Fe_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Fe_norm<-resid(Fe_area_norm)
+Fe_area_norm<-lm(log10(Fe_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Fe_norm<-resid(Fe_area_norm)
 
-K_area_norm<-lm(log10(K_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$K_norm<-resid(K_area_norm)
+K_area_norm<-lm(log10(K_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$K_norm<-resid(K_area_norm)
 
-Mg_area_norm<-lm(log10(Mg_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Mg_norm<-resid(Mg_area_norm)
+Mg_area_norm<-lm(log10(Mg_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Mg_norm<-resid(Mg_area_norm)
 
-meta(all.ref)$Mn_area_omit<-meta(all.ref)$Mn_area
-meta(all.ref)$Mn_area_omit[which(meta(all.ref)$Mn_area_omit==0)]<-NA
-Mn_area_norm<-lm(log10(Mn_area_omit)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Mn_norm<-resid(Mn_area_norm)
-meta(all.ref)$Mn_area_omit<-NULL
+meta(ref.traits)$Mn_area_omit<-meta(ref.traits)$Mn_area
+meta(ref.traits)$Mn_area_omit[which(meta(ref.traits)$Mn_area_omit==0)]<-NA
+Mn_area_norm<-lm(log10(Mn_area_omit)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Mn_norm<-resid(Mn_area_norm)
+meta(ref.traits)$Mn_area_omit<-NULL
 
-meta(all.ref)$Na_area_omit<-meta(all.ref)$Na_area
-meta(all.ref)$Na_area_omit[which(meta(all.ref)$Na_area_omit==0)]<-NA
-Na_area_norm<-lm(log10(Na_area_omit)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Na_norm<-resid(Na_area_norm)
-meta(all.ref)$Na_area_omit<-NULL
+meta(ref.traits)$Na_area_omit<-meta(ref.traits)$Na_area
+meta(ref.traits)$Na_area_omit[which(meta(ref.traits)$Na_area_omit==0)]<-NA
+Na_area_norm<-lm(log10(Na_area_omit)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Na_norm<-resid(Na_area_norm)
+meta(ref.traits)$Na_area_omit<-NULL
 
-P_area_norm<-lm(log10(P_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$P_norm<-resid(P_area_norm)
+P_area_norm<-lm(log10(P_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$P_norm<-resid(P_area_norm)
 
-Zn_area_norm<-lm(log10(Zn_area)~log10(0.1*LMA),data=meta(all.ref),na.action=na.exclude)
-meta(all.ref)$Zn_norm<-resid(Zn_area_norm)
+Zn_area_norm<-lm(log10(Zn_area)~log10(0.1*LMA),data=meta(ref.traits),na.action=na.exclude)
+meta(ref.traits)$Zn_norm<-resid(Zn_area_norm)
+
+################################################
+## PCA of traits for trait modeling paper (not Grime Review)
+
+trait.pca<-prcomp(~.,meta(ref.traits)[,c("LMA","LDMC","EWT","Cmass","Nmass",
+                                      "solubles_mass","hemicellulose_mass",
+                                      "cellulose_mass","lignin_mass","chlA_mass",
+                                      "chlB_mass","car_mass")],
+                  scale.=T,na.action = na.exclude)
+
+trait.pca.val <- data.frame(functional.group=meta(ref.traits)$functional.group,trait.pca$x)
+trait.pca.loadings<-data.frame(variables = rownames(trait.pca$rotation), trait.pca$rotation)
+trait.pca.loadings$variables<-gsub("_mass","",trait.pca.loadings$variables)
+trait.pca.loadings$variables<-gsub("mass","",trait.pca.loadings$variables)
+trait.pca.perc<-trait.pca$sdev^2/sum(trait.pca$sdev^2)*100
+
+trait.pca.plot<-ggplot(trait.pca.val, aes(x = PC1, y = PC2, color = functional.group)) +
+  geom_point(size = 2,alpha=0.6) +
+  geom_segment(data = trait.pca.loadings, aes(x = 0, y = 0, xend = PC1*10, yend = PC2*10),
+               arrow = arrow(length = unit(1/2, "picas")),
+               color = "black",size=1) +
+  annotate("text",
+           x = trait.pca.loadings$PC1*12+c(0,0,0,0,0.3,0,0,0,0,-0.4,0.3,0),
+           y = trait.pca.loadings$PC2*12+c(0,0,0,0,-0.4,0,0,0,0,0.3,-0.1,0),
+           label = trait.pca.loadings$variables)+
+  theme_bw()+theme(text=element_text(size=15))+
+  guides(color=guide_legend("Functional group"))+
+  coord_fixed(ratio=trait.pca.perc[2]/trait.pca.perc[1])+
+  labs(x=paste("PC1 (",round(trait.pca.perc[1],1),"% variance)",sep=""),
+       y=paste("PC2 (",round(trait.pca.perc[2],1),"% variance)",sep=""))+
+  scale_color_manual(values=colorBlind)
+
+traitnorm.pca<-prcomp(~.,meta(ref.traits)[,c("LMA","LDMC","EWT","Cnorm","Nnorm",
+                                          "hemicellulose_norm","cellulose_norm",
+                                          "lignin_norm","chlA_norm")],
+                      scale.=T,na.action = na.exclude)
+
+traitnorm.pca.val <- data.frame(functional.group=meta(ref.traits)$functional.group,traitnorm.pca$x)
+traitnorm.pca.loadings<-data.frame(variables = rownames(traitnorm.pca$rotation), traitnorm.pca$rotation)
+traitnorm.pca.loadings$variables<-gsub("_norm","",traitnorm.pca.loadings$variables)
+traitnorm.pca.loadings$variables<-gsub("norm","",traitnorm.pca.loadings$variables)
+traitnorm.pca.perc<-traitnorm.pca$sdev^2/sum(traitnorm.pca$sdev^2)*100
+
+traitnorm.pca.plot<-ggplot(traitnorm.pca.val, aes(x = -PC1*2, y = -PC2*2, color = functional.group)) +
+  geom_point(size = 2,alpha=0.6) +
+  geom_segment(data = traitnorm.pca.loadings, aes(x = 0, y = 0, xend = -PC1*10, yend = -PC2*10),
+               arrow = arrow(length = unit(1/2, "picas")),
+               color = "black",size=1) +
+  annotate("text", x = -traitnorm.pca.loadings$PC1*13, y = -traitnorm.pca.loadings$PC2*13,
+           label = traitnorm.pca.loadings$variables)+
+  theme_bw()+theme(text=element_text(size=15))+
+  guides(color=guide_legend("Functional group"))+
+  coord_fixed(ratio=traitnorm.pca$sdev[2]^2/traitnorm.pca$sdev[1]^2)+
+  labs(x=paste("PC1 (",round(traitnorm.pca.perc[1],1),"% variance)",sep=""),
+       y=paste("PC2 (",round(traitnorm.pca.perc[2],1),"% variance)",sep=""))+
+  scale_color_manual(values=colorBlind)
+
+pdf("Images/pca_plot.pdf")
+trait.pca.plot+traitnorm.pca.plot+plot_layout(ncol=1,guides="collect") &
+  theme(legend.position = "bottom")
+dev.off()
+
+#################################################
+## preparing data for dimensionality analyses
 
 ## log-transform LMA and EWT to reduce skewness
 meta(ref.traits)$logLMA<-log(meta(ref.traits)$LMA)
@@ -137,9 +202,9 @@ select.bands<-c(seq(from=400,to=780,by=20),
 #################################################
 ## factor analysis of spectra
 
-ref.traits.ref<-as.matrix(ref.traits.samp)
-colnames(ref.traits.ref)<-paste("X",colnames(ref.traits.ref),sep="")
-ref.pca<-prcomp(~.,data=as.data.frame(ref.traits.ref),scale.=T)
+ref.traits.mat<-as.matrix(ref.traits.samp)
+colnames(ref.traits.mat)<-paste("X",colnames(ref.traits.mat),sep="")
+ref.pca<-prcomp(~.,data=as.data.frame(ref.traits.mat),scale.=T)
 meta(ref.traits.samp)$PC1<-ref.pca$x[,1]
 meta(ref.traits.samp)$PC2<-ref.pca$x[,2]
 meta(ref.traits.samp)$PC3<-ref.pca$x[,3]
@@ -149,7 +214,7 @@ meta(ref.traits.samp)$PC4<-ref.pca$x[,4]
 # plot(400:2400,ref.pca$center,type="l")
 ## scree plot
 screeplot(ref.pca)
-screeplot(prcomp(~.,data=as.data.frame(ref.traits.ref[,select.bands-399]),scale.=T))
+screeplot(prcomp(~.,data=as.data.frame(ref.traits.mat[,select.bands-399]),scale.=T))
 screeplot(prcomp(~.,data=as.data.frame(traits.ni.norm),scale.=T))
 
 ## reconstructing the spectrum
@@ -309,12 +374,8 @@ lbmle(x = ref.traits.norm[,select.bands-399], k1=5, k2=10)
 lbmle(x = na.omit(traits.ni.norm), k1=5, k2=10)
 mada(x = ref.traits.norm[,select.bands-399], k = NULL, comb = "average", maxDim = 10)
 mada(x = na.omit(traits.ni.norm), k = NULL, comb = "average", maxDim = 10)
-side(x = ref.traits.norm[,select.bands-399], comb = "average", maxDim = 10)
-side(x = na.omit(traits.ni.norm), comb = "average", maxDim = 10)
 nni(x = ref.traits.norm[,select.bands-399], k1 = 5, k2 = 30, eps = 0.01, p = NULL)
 nni(x = na.omit(traits.ni.norm), k1 = 5, k2 = 30, eps = 0.01, p = NULL)
-dancoDimEst(data = ref.traits.norm[,select.bands-399], k=5, D=10, ver = "DANCo")
-dancoDimEst(data = na.omit(traits.ni.norm), k=5, D=10, ver = "DANCo")
 dancoDimEst(data = ref.traits.norm[,select.bands-399], k=5, D=10, ver = "MIND_MLi")
 dancoDimEst(data = na.omit(traits.ni.norm), k=5, D=10, ver = "MIND_MLi")
 
@@ -331,9 +392,9 @@ traits.lle<-lle(na.omit(traits.norm),m=10,k=8,id = T,iLLE=T)
 #########################################
 ## dimensionality of fresh, pressed, and ground spectra
 
-fresh.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/fresh_spec_EL_agg.rds")
-pressed.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/pressed_spec_EL_agg.rds")
-ground.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/ground_spec_EL_agg.rds")
+fresh.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/fresh_spec_all.rds")
+pressed.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/pressed_spec_all.rds")
+ground.spec<-readRDS("../../TraitModels2018/HerbariumPaper/ProcessedSpectralData/ground_spec_all.rds")
 
 meta(fresh.spec)$chl<-meta(fresh.spec)$chlA+meta(fresh.spec)$chlB
 
@@ -379,11 +440,6 @@ mada(x = fresh.spec.norm[,select.bands-399], k = NULL, comb = "average", maxDim 
 mada(x = pressed.spec.norm[,select.bands-399], k = NULL, comb = "average", maxDim = 10)
 mada(x = ground.spec.norm[,select.bands-399], k = NULL, comb = "average", maxDim = 10)
 mada(x = na.omit(traits.sub), k = NULL, comb = "average", maxDim = 10)
-
-side(x = fresh.spec.norm[,select.bands-399],maxDim=10)
-side(x = pressed.spec.norm[,select.bands-399],maxDim=10)
-side(x = ground.spec.norm[,select.bands-399],maxDim=10)
-side(x = na.omit(traits.sub),maxDim=10)
 
 nni(x = fresh.spec.norm[,select.bands-399], k1 = 5, k2 = 30, eps = 0.01, p = NULL)
 nni(x = pressed.spec.norm[,select.bands-399], k1 = 5, k2 = 30, eps = 0.01, p = NULL)
@@ -443,7 +499,6 @@ corint(x=prospect.sim.norm[,select.bands-399],p=20)
 ## ML; not sure about correct choice of p or k1/k2
 lbmle(x = prospect.sim.norm[,select.bands-399], k1=5, k2=10)
 mada(x = prospect.sim.norm[,select.bands-399], k = NULL, comb = "average", maxDim = 10)
-side(x = prospect.sim.norm[,select.bands-399],maxDim=10)
 nni(x = prospect.sim.norm[,select.bands-399], k1 = 5, k2 = 30, eps = 0.01, p = NULL)
 dancoDimEst(data = prospect.sim.norm[,select.bands-399], k=5, D=10, ver = "MIND_MLi")
 
@@ -458,9 +513,11 @@ plot(prospect.isomap$points[,1]~prospect.isomap$points[,2])
 ############################################
 ## trait covariance simulation
 
+## pick the sizes of functional groups
 FG.sizes<-c(50,50,50,25,25)
 FG<-c(rep("FG1",50),rep("FG2",50),rep("FG3",50),rep("FG4",25),rep("FG5",25))
 
+## generate LMA distribution for each functional group
 Cm1<-rtruncnorm(50,a=0.0008,mean=quantile(meta(ref.traits.samp)$LMA/10,probs=0.125),sd=0.001)
 Cm2<-rtruncnorm(50,a=0.0008,mean=quantile(meta(ref.traits.samp)$LMA/10,probs=0.375),sd=0.0015)
 Cm3<-rtruncnorm(50,a=0.0008,mean=quantile(meta(ref.traits.samp)$LMA/10,probs=0.625),sd=0.002)
@@ -468,6 +525,7 @@ Cm4<-rtruncnorm(25,a=0.0008,mean=quantile(meta(ref.traits.samp)$LMA/10,probs=0.7
 Cm5<-rtruncnorm(25,a=0.0008,mean=quantile(meta(ref.traits.samp)$LMA/10,probs=0.875),sd=0.003)
 Cm<-c(Cm1,Cm2,Cm3,Cm4,Cm5)
 
+## set constant values for the other functional groups
 N<-1.5
 Cw<-0.007
 Cab<-40
@@ -476,9 +534,11 @@ prospect.params$Car<-prospect.params$Cab/6.24 ## mean chl:car ratio
 prospect.params$Anth<-0
 prospect.params$Cbrown<-0
 
+## palette
 colorBlind  <- c("#E69F00","#009E73","#56B4E9","#F0E442",
                  "#0072B2","#CC79A7","#D55E00","#999999")
 
+## plot functional group LMA distributions
 Cm_density<-ggplot(data=prospect.params,
                    aes(x=Cm,color=FG))+
   geom_density(size=1.5)+
@@ -491,6 +551,7 @@ Cm_density<-ggplot(data=prospect.params,
   scale_color_manual(values=colorBlind[c(1,2,4,5,6)])+
   guides(color=F)
 
+## simulate spectra with PROSPECT
 prospect.sim<-t(apply(prospect.params,1,
                       function(x) {
                         spec.sim<-PROSPECT(N=x["N"],Cab=x["Cab"],Car=x["Car"],
@@ -508,6 +569,7 @@ prospect.spec.df<-data.frame(sample_id=names(prospect.spec),
 prospect.spec.long<-melt(prospect.spec.df,id.vars = c("sample_id","FG"))
 prospect.spec.long$variable<-as.numeric(gsub(pattern = "X",replacement = "",x = prospect.spec.long$variable))
 
+## plot spectra per functional group
 prospect_fg_spec_plot<-ggplot(prospect.spec.long,
                               aes(x=variable,y=value,color=FG))+
   stat_summary(fun=median,na.rm=T,geom="line",size=1)+
@@ -519,12 +581,17 @@ prospect_fg_spec_plot<-ggplot(prospect.spec.long,
   scale_x_continuous(expand = c(0, 0),limits=c(390,2510))+
   scale_color_manual(values=colorBlind[c(1,2,4,5,6)])
 
+## generate other traits
+## create T1 by adding noise and then constraining it to the same range as LMA
 protoT1<-prospect.params$Cm+rnorm(200,mean=0,sd=0.003)
 prospect.params$T1<-(max(prospect.params$Cm)-min(prospect.params$Cm))*(protoT1-min(protoT1))/(max(protoT1)-min(protoT1))+min(prospect.params$Cm)
+
+## T2 is based on functional group means rather than the individual values
 prospect.params$FG.mean<-unlist(apply(prospect.params,1,function(x) median(prospect.params[["Cm"]][prospect.params[["FG"]]==x[["FG"]]])))
 protoT2<-prospect.params$FG.mean+rnorm(200,mean=0,sd=0.001)
 prospect.params$T2<-(max(prospect.params$Cm)-min(prospect.params$Cm))*(protoT2-min(protoT2))/(max(protoT2)-min(protoT2))+min(prospect.params$Cm)
 
+## plot relationship between synthetic traits and LMA
 T1_Cm_plot<-ggplot(prospect.params,aes(y=T1,x=Cm,color=FG))+
   geom_point(size=2)+geom_smooth(method="lm",se=F)+
   theme_bw()+
@@ -547,6 +614,7 @@ T2_Cm_plot<-ggplot(prospect.params,aes(y=T2,x=Cm,color=FG))+
   scale_color_manual(values=colorBlind[c(1,2,4,5,6)])+
   guides(color=F)
 
+## just to show we can predict LMA from the spectrum
 Cm_model<-plsr(prospect.params$Cm~as.matrix(prospect.sim),
                ncomp=30,method = "oscorespls",validation="CV",segments=10)
 ncomp_Cm_model <- selectNcomp(Cm_model, method = "onesigma", plot = FALSE)
@@ -564,6 +632,7 @@ ggplot(Cm_pred,aes(y=measured,x=val_pred,color=FG))+
   labs(y="Measured",x="Predicted")+
   scale_color_manual(values=colorBlind[c(1,2,4,5,6)])
 
+## can we predict T1 from the spectrum?
 T1_model<-plsr(prospect.params$T1~as.matrix(prospect.sim),
                ncomp=30,method = "oscorespls",validation="CV",segments=10)
 ncomp_T1_model <- selectNcomp(T1_model, method = "onesigma", plot = FALSE)
@@ -582,6 +651,7 @@ T1_plot<-ggplot(T1_pred,aes(y=measured,x=val_pred,color=FG))+
   scale_color_manual(values=colorBlind[c(1,2,4,5,6)])+
   guides(color=F)
 
+## can we predict T2 from the spectrum?
 T2_model<-plsr(prospect.params$T2~as.matrix(prospect.sim),
                ncomp=30,method = "oscorespls",validation="CV",segments=10)
 ncomp_T2_model <- selectNcomp(T2_model, method = "onesigma", plot = FALSE)
@@ -608,5 +678,6 @@ Cm_density+prospect_fg_spec_plot+
   theme(legend.position = "bottom")
 dev.off()
 
+## just to see what the VIP looks like...
 source("Scripts/VIP.R")
 plot(400:2500,VIP(T2_model)[ncomp_T2_model,])
