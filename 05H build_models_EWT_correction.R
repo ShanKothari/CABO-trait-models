2eb77d82e9f649e0a46f716de42ebc9384c58f25
Shan Kothari <quercusacerifolia@gmail.com>
2023-12-26 16:09:12 -0500

    plot full figures for correction

--- 05H build_models_EWT_correction.R
+++ 05H build_models_EWT_correction.R
@@ -309,6 +309,9 @@ EWT_actual.jack.df.ref<-readRDS("SavedResults/EWT_corrected_jack_df_ref.rds")
 EWT_actual.jack.df.trans<-readRDS("SavedResults/EWT_corrected_jack_df_trans.rds")
 EWT_actual.jack.df.abs<-readRDS("SavedResults/EWT_corrected_jack_df_abs.rds")
 
+source("Scripts/CABO-trait-models/06A plotting_models_ref.R")
+source("Scripts/CABO-trait-models/06B plotting_models_combined.R")
+
 all.EWT<-c(EWT_actual.jack.df.ref$Measured,
            EWT_actual.jack.df.ref$pred.mean,
            EWT_actual.jack.df.trans$pred.mean,
@@ -316,8 +319,32 @@ all.EWT<-c(EWT_actual.jack.df.ref$Measured,
 EWT_upper<-max(all.EWT,na.rm=T)+0.02
 EWT_lower<-min(all.EWT,na.rm=T)-0.02
 
-EWT_actual.ref.val.plot<-ggplot(EWT_actual.jack.df.ref,
-                         aes(y=Measured,x=pred.mean,color=functional.group))+
+EWT_actual.ref.val.plot1<-ggplot(EWT_actual.jack.df.ref,
+                                 aes(y=Measured,x=pred.mean,color=functional.group))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  coord_cartesian(xlim=c(EWT_lower,EWT_upper),
+                  ylim=c(EWT_lower,EWT_upper))+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.25))+
+  labs(y="Measured EWT (mm)",x="Predicted EWT (mm)")+
+  guides(color=F)+
+  scale_color_manual(values=colorBlind)
+
+pdf("Images/val_plots_ref1_corrected.pdf",width = 16,height = 19)
+(LMA.val.plot+LDMC.val.plot+EWT_actual.ref.val.plot1)/
+  (Nmass.val.plot+Cmass.val.plot+solubles_mass.val.plot)/
+  (hemicellulose_mass.val.plot+cellulose_mass.val.plot+lignin_mass.val.plot)/
+  (chlA_mass.val.plot+chlB_mass.val.plot+car_mass.val.plot) &
+  plot_layout(guides="collect") & theme(legend.position = "bottom")
+dev.off()
+
+## larger text
+EWT_actual.ref.val.plot2<-ggplot(EWT_actual.jack.df.ref,
+                                 aes(y=Measured,x=pred.mean,color=functional.group))+
   geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
                  color="gray")+
   geom_point(size=2,alpha=0.7)+geom_smooth(method="lm",se=F)+
@@ -329,8 +356,7 @@ EWT_actual.ref.val.plot<-ggplot(EWT_actual.jack.df.ref,
         legend.position = c(0.8, 0.25))+
   labs(y="Measured EWT (mm)",x="Predicted EWT (mm)")+
   guides(color=F)+
-  scale_color_manual(values=colorBlind)+
-  ggtitle("Reflectance")
+  scale_color_manual(values=colorBlind)
 
 EWT_actual.trans.val.plot<-ggplot(EWT_actual.jack.df.trans,
                            aes(y=Measured,x=pred.mean,color=functional.group))+
@@ -347,8 +373,7 @@ EWT_actual.trans.val.plot<-ggplot(EWT_actual.jack.df.trans,
         axis.text.y = element_blank())+
   labs(y="Measured EWT (mm)",x="Predicted EWT (mm)")+
   guides(color=F)+
-  scale_color_manual(values=colorBlind)+
-  ggtitle("Transmittance")
+  scale_color_manual(values=colorBlind)
 
 EWT_actual.abs.val.plot<-ggplot(EWT_actual.jack.df.abs,
                          aes(y=Measured,x=pred.mean,color=functional.group))+
@@ -365,11 +390,12 @@ EWT_actual.abs.val.plot<-ggplot(EWT_actual.jack.df.abs,
         axis.text.y = element_blank())+
   labs(y="Measured EWT (mm)",x="Predicted EWT (mm)",
        color="Functional group")+
-  scale_color_manual(values=colorBlind)+
-  ggtitle("Absorptance")
+  scale_color_manual(values=colorBlind)
 
-pdf("Images/EWT_corrected_val_plot.pdf",width=16,height=7)
-  (EWT_actual.ref.val.plot + EWT_actual.trans.val.plot + EWT_actual.abs.val.plot) &
+pdf("Images/val_plots_comp1_corrected.pdf",width=16,height=17)
+(LMA.ref.val.plot + LMA.trans.val.plot + LMA.abs.val.plot) /
+  (LDMC.ref.val.plot + LDMC.trans.val.plot + LDMC.abs.val.plot) /
+  (EWT_actual.ref.val.plot2 + EWT_actual.trans.val.plot + EWT_actual.abs.val.plot) &
   plot_layout(guides="collect") & theme(legend.position = "bottom")
 dev.off()
 
@@ -384,6 +410,7 @@ with(EWT_actual.jack.df.abs,
 
 source("Scripts/CABO-trait-models/00 useful_functions.R")
 EWT_actual.jack.coefs.ref<-readRDS("SavedResults/EWT_corrected_jack_coefs_ref.rds")
+ind_val_list<-readRDS("SavedResults/ind_val_list.rds")
 
 ## read in processed data
 LOPEX<-readRDS("IndependentValidationData/LOPEX/LOPEX_processed.rds")
@@ -392,6 +419,62 @@ Dessain.spec<-readRDS("IndependentValidationData/Dessain_processed.rds")
 
 LOPEX_ANGERS<-spectrolab::combine(LOPEX,ANGERS)
 all_val_ref<-spectrolab::combine(LOPEX_ANGERS,Dessain.spec)
+
+solubles_pred_df<-ind_val_list$solubles_mass
+solubles_all<-with(solubles_pred_df,c(pred.low[!is.na(Measured)],
+                                      pred.high[!is.na(Measured)],
+                                      Measured))
+solubles_upper<-max(solubles_all,na.rm=T)+1
+solubles_lower<-min(solubles_all,na.rm=T)-1
+
+hemicellulose_pred_df<-ind_val_list$hemicellulose_mass
+hemicellulose_all<-with(hemicellulose_pred_df,c(pred.low[!is.na(Measured)],
+                                                pred.high[!is.na(Measured)],
+                                                Measured))
+hemicellulose_upper<-max(hemicellulose_all,na.rm=T)+1
+hemicellulose_lower<-min(hemicellulose_all,na.rm=T)-1
+
+cellulose_pred_df<-ind_val_list$cellulose_mass
+cellulose_all<-with(cellulose_pred_df,c(pred.low[!is.na(Measured)],
+                                        pred.high[!is.na(Measured)],
+                                        Measured))
+cellulose_upper<-max(cellulose_all,na.rm=T)+1
+cellulose_lower<-min(cellulose_all,na.rm=T)-1
+
+lignin_pred_df<-ind_val_list$lignin_mass
+lignin_all<-with(lignin_pred_df,c(pred.low[!is.na(Measured)],
+                                  pred.high[!is.na(Measured)],
+                                  Measured))
+lignin_upper<-max(lignin_all,na.rm=T)+1
+lignin_lower<-min(lignin_all,na.rm=T)-1
+
+Nmass_pred_df<-ind_val_list$Nmass
+Nmass_all<-with(Nmass_pred_df,c(pred.low[!is.na(Measured)],
+                                pred.high[!is.na(Measured)],
+                                Measured))
+Nmass_upper<-max(Nmass_all,na.rm=T)+0.1
+Nmass_lower<-min(Nmass_all,na.rm=T)-0.1
+
+Cmass_pred_df<-ind_val_list$Cmass
+Cmass_all<-with(Cmass_pred_df,c(pred.low[!is.na(Measured)],
+                                pred.high[!is.na(Measured)],
+                                Measured))
+Cmass_upper<-max(Cmass_all,na.rm=T)+1
+Cmass_lower<-min(Cmass_all,na.rm=T)-1
+
+LMA_pred_df<-ind_val_list$LMA
+LMA_all<-with(LMA_pred_df,c(pred.low[!is.na(Measured)],
+                            pred.high[!is.na(Measured)],
+                            Measured))
+LMA_upper<-max(LMA_all,na.rm=T)+0.02
+LMA_lower<-min(LMA_all,na.rm=T)-0.02
+
+LDMC_pred_df<-ind_val_list$LDMC
+LDMC_all<-with(LDMC_pred_df,c(pred.low[!is.na(Measured)],
+                              pred.high[!is.na(Measured)],
+                              Measured))
+LDMC_upper<-max(LDMC_all,na.rm=T)+10
+LDMC_lower<-min(LDMC_all,na.rm=T)-10
 
 EWT_pred<-apply.coefs(EWT_actual.jack.coefs.ref,val.spec = all_val_ref)
 EWT_stat<-t(apply(EWT_pred,1,
@@ -406,6 +489,146 @@ EWT_all<-with(EWT_pred_df,c(pred.low[!is.na(Measured)],
                             Measured))
 EWT_upper<-max(EWT_all,na.rm=T)+0.03
 EWT_lower<-min(EWT_all,na.rm=T)-0.03
+
+chlA_pred_df<-ind_val_list$chlA
+chlA_all<-with(chlA_pred_df,c(pred.low[!is.na(Measured)],
+                              pred.high[!is.na(Measured)],
+                              Measured))
+chlA_upper<-max(chlA_all,na.rm=T)+0.5
+chlA_lower<-min(chlA_all,na.rm=T)-0.5
+
+chlB_pred_df<-ind_val_list$chlB
+chlB_all<-with(chlB_pred_df,c(pred.low[!is.na(Measured)],
+                              pred.high[!is.na(Measured)],
+                              Measured))
+chlB_upper<-max(chlB_all,na.rm=T)+0.3
+chlB_lower<-min(chlB_all,na.rm=T)-0.3
+
+car_pred_df<-ind_val_list$car
+car_all<-with(car_pred_df,c(pred.low[!is.na(Measured)],
+                            pred.high[!is.na(Measured)],
+                            Measured))
+car_upper<-max(car_all,na.rm=T)+0.2
+car_lower<-min(car_all,na.rm=T)-0.2
+
+colorBlind  <- c("#E69F00","#009E73","#56B4E9","#F0E442",
+                 "#0072B2","#CC79A7","#D55E00","#999999")
+
+solubles_ind_val<-ggplot(data=solubles_pred_df,
+                         aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y="Measured solubles (%)",x="Predicted solubles (%)")+
+  coord_cartesian(xlim=c(solubles_lower,solubles_upper),
+                  ylim=c(solubles_lower,solubles_upper))+
+  scale_color_manual(values=colorBlind)
+
+hemicellulose_ind_val<-ggplot(data=hemicellulose_pred_df,
+                              aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y="Measured hemicellulose (%)",x="Predicted hemicellulose (%)")+
+  coord_cartesian(xlim=c(hemicellulose_lower,hemicellulose_upper),
+                  ylim=c(hemicellulose_lower,hemicellulose_upper))+
+  scale_color_manual(values=colorBlind)
+
+cellulose_ind_val<-ggplot(data=cellulose_pred_df,
+                          aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y="Measured cellulose (%)",x="Predicted cellulose (%)")+
+  coord_cartesian(xlim=c(cellulose_lower,cellulose_upper),
+                  ylim=c(cellulose_lower,cellulose_upper))+
+  scale_color_manual(values=colorBlind)
+
+lignin_ind_val<-ggplot(data=lignin_pred_df,
+                       aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y="Measured lignin (%)",x="Predicted lignin (%)")+
+  coord_cartesian(xlim=c(lignin_lower,lignin_upper),
+                  ylim=c(lignin_lower,lignin_upper))+
+  scale_color_manual(values=colorBlind)
+
+Nmass_ind_val<-ggplot(data=Nmass_pred_df,
+                      aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured N (%)"),
+       x=expression("Predicted N (%)"))+
+  coord_cartesian(xlim=c(Nmass_lower,Nmass_upper),
+                  ylim=c(Nmass_lower,Nmass_upper))+
+  scale_color_manual(values=colorBlind)
+
+Cmass_ind_val<-ggplot(data=Cmass_pred_df,
+                      aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured C (%)"),
+       x=expression("Predicted C (%)"))+
+  coord_cartesian(xlim=c(Cmass_lower,Cmass_upper),
+                  ylim=c(Cmass_lower,Cmass_upper))+
+  scale_color_manual(values=colorBlind)
+
+LMA_ind_val<-ggplot(data=LMA_pred_df,
+                    aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured LMA (kg m"^-2*")"),
+       x=expression("Predicted LMA (kg m"^-2*")"))+
+  coord_cartesian(xlim=c(LMA_lower,LMA_upper),
+                  ylim=c(LMA_lower,LMA_upper))+
+  scale_color_manual(values=colorBlind)
+
+LDMC_ind_val<-ggplot(data=LDMC_pred_df,
+                     aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured LDMC (mg g"^-1*")"),
+       x=expression("Predicted LDMC (mg g"^-1*")"))+
+  coord_cartesian(xlim=c(LDMC_lower,LDMC_upper),
+                  ylim=c(LDMC_lower,LDMC_upper))+
+  scale_color_manual(values=colorBlind)
 
 EWT_ind_val<-ggplot(data=EWT_pred_df,
                     aes(x=pred.mean,y=Measured,color=dataset))+
@@ -422,8 +645,59 @@ EWT_ind_val<-ggplot(data=EWT_pred_df,
   scale_color_manual(values=colorBlind)+
   guides(color=guide_legend(title="Dataset"))
 
-pdf("Images/EWT_corrected_ind_val_plot.pdf",height=6,width=6)
-EWT_ind_val & theme(legend.position = "bottom")
+chlA_ind_val<-ggplot(data=chlA_pred_df,
+                     aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured Chl"~italic("a")~"(mg g"^-1*")"),
+       x=expression("Predicted Chl"~italic("a")~"(mg g"^-1*")"))+
+  coord_cartesian(xlim=c(chlA_lower,chlA_upper),
+                  ylim=c(chlA_lower,chlA_upper))+
+  scale_color_manual(values=colorBlind)
+
+chlB_ind_val<-ggplot(data=chlB_pred_df,
+                     aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured Chl"~italic("b")~"(mg g"^-1*")"),
+       x=expression("Predicted Chl"~italic("b")~"(mg g"^-1*")"))+
+  coord_cartesian(xlim=c(chlB_lower,chlB_upper),
+                  ylim=c(chlB_lower,chlB_upper))+
+  scale_color_manual(values=colorBlind)
+
+car_ind_val<-ggplot(data=car_pred_df,
+                    aes(x=pred.mean,y=Measured,color=dataset))+
+  geom_errorbarh(aes(y=Measured,xmin=pred.low,xmax=pred.high),
+                 color="gray")+
+  geom_point(size=2,alpha=0.7)+
+  theme_bw()+
+  theme(text = element_text(size=20))+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  geom_smooth(method="lm",se=F)+
+  labs(y=expression("Measured carotenoids (mg g"^-1*")"),
+       x=expression("Predicted carotenoids (mg g"^-1*")"))+
+  coord_cartesian(xlim=c(car_lower,car_upper),
+                  ylim=c(car_lower,car_upper))+
+  scale_color_manual(values=colorBlind)
+
+pdf("Images/ind_val_plots1_corrected.pdf",width = 16,height = 19, onefile=F)
+ggarrange(plotlist=list(solubles_ind_val,hemicellulose_ind_val,
+                        cellulose_ind_val,lignin_ind_val,
+                        Nmass_ind_val,Cmass_ind_val,
+                        LMA_ind_val,LDMC_ind_val,EWT_ind_val,
+                        chlA_ind_val,chlB_ind_val,car_ind_val),
+          common.legend = T,legend = "bottom",
+          nrow=4,ncol=3)
 dev.off()
 
 summary(lm(Measured~pred.mean,data=EWT_pred_df[EWT_pred_df$dataset=="ANGERS",]))
